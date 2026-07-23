7721a46c1118358abdce4397deba83581d16b766
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-07-24 05:49:30 +0800

    Update CustomShareIconRootListController.h

--- customshareiconprefs/CustomShareIconRootListController.h
+++ customshareiconprefs/CustomShareIconRootListController.h
@@ -1,1 +1,6 @@
+#import <Preferences/PSListController.h>
+#import <PhotosUI/PhotosUI.h>
 
+@interface CustomShareIconRootListController : PSListController <PHPickerViewControllerDelegate>
+@property (nonatomic, strong) NSString *pendingBundleID;
+@end
