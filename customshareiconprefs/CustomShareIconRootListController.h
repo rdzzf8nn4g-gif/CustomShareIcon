3b367c2ca98caa0bf6cb98c17c493bea83262990
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-07-25 23:42:02 +0800

    Update CustomShareIconRootListController.h

--- customshareiconprefs/CustomShareIconRootListController.h
+++ customshareiconprefs/CustomShareIconRootListController.h
@@ -4,11 +4,9 @@
 @interface CustomShareIconRootListController : PSListController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (nonatomic, strong) NSMutableDictionary *iconsDict;
 @property (nonatomic, copy) NSString *pendingBundleID;
-
 - (void)loadIconsFromPrefs;
 - (void)saveIconsToPrefs;
 - (void)notifyReload;
 - (void)addNewIcon;
 - (void)pickImageForKey:(NSString *)key;
-
 @end
